e20399480ed2ba18fe3a7aac0e0fd476599887b8
viceshark <96300634+viceshark@users.noreply.github.com>
2026-06-09 18:41:37 +0300

    Add: search-in-rotated-sorted-array-ii - submission-3

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length -1;
+
+        //Сначала надо понять больше или меньше мида наш таргет
+        // Потом надо понять есть ли свертка
+        // Если есть, то дальше развилка если таргет больше хая, то идем влево от мида, если меньше ищем между мид и хай
+
+        while(low <= high){
+
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){return true;}
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+            low++;
+            high--;}
+            else if(nums[mid] >= nums[low]){
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid-1;
+                }
+                else low = mid + 1;
+            }
+            else {
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid+1;
+                }
+                else high = mid - 1;
+            }
+
+        }
+
+        return false;
+    }
+}
